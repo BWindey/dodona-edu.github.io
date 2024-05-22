8e1dfd5ea71650f6af4bf9c0178339ab68f3bca9
Bram Windey <windey.bram@gmail.com>
2024-05-23 00:27:46 +0200

    Simplified elements

--- .vitepress/theme/PermissionSquares.vue
+++ .vitepress/theme/PermissionSquares.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="display: flex; justify-content: center; gap: 4px;">
-    <span v-if="level >= 1" style="display: inline-block; width: 10px; height: 10px; background-color: red; border-radius: 2px;"></span>
-    <span v-if="level >= 2" style="display: inline-block; width: 10px; height: 10px; background-color: orange; border-radius: 2px;"></span>
-    <span v-if="level >= 3" style="display: inline-block; width: 10px; height: 10px; background-color: green; border-radius: 2px;"></span>
+    <div v-if="level >= 1" style="width: 10px; height: 10px; background-color: red; border-radius: 2px;"></div>
+    <div v-if="level >= 2" style="width: 10px; height: 10px; background-color: orange; border-radius: 2px;"></div>
+    <div v-if="level >= 3" style="width: 10px; height: 10px; background-color: green; border-radius: 2px;"></div>
   </div>
 </template>
 
