d09693324bcd297427b53ea3e4e6d4a6e8cf9926
Bram Windey <windey.bram@gmail.com>
2024-05-23 00:18:21 +0200

    Remove svg's to use inline span html blocks, and removed template_index.md as I don't need it anymore with this nice PermissionSquares thing

--- .vitepress/theme/PermissionSquares.vue
+++ .vitepress/theme/PermissionSquares.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="display: flex; justify-content: center;">
-    <img v-if="level >= 1" src="/red_square.svg" alt="Admin" style="margin-right: 5px" />
-    <img v-if="level >= 2" src="/orange_square.svg" alt="Teacher" style="margin-right: 5px" />
-    <img v-if="level >= 3" src="/green_square.svg" alt="Student" style="margin-right: 5px" />
+  <div style="display: flex; justify-content: center; gap: 4px;">
+    <span v-if="level >= 1" style="display: inline-block; width: 10px; height: 10px; background-color: red; border-radius: 2px;"></span>
+    <span v-if="level >= 2" style="display: inline-block; width: 10px; height: 10px; background-color: orange; border-radius: 2px;"></span>
+    <span v-if="level >= 3" style="display: inline-block; width: 10px; height: 10px; background-color: green; border-radius: 2px;"></span>
   </div>
 </template>
 
